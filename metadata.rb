842eddb0074695338c8abfdb9ae0ae7e48569215
luis rodriguez <luis.rodriguez@cru.org>
2014-08-13 17:13:38 -0400

    move cookbooks to root

--- metadata.rb
+++
@@ -1,9 +0,0 @@
-name	'test_workstation'
-maintainer	'Luis Rodriguez'
-maintainer_email 'luis.rodriguez@cru.org'
-license	'Apache 2.0'
-description	'Setup workstation'
-long_description	'none'
-version	'0.1.0'
-
-depends 'wildfly'
